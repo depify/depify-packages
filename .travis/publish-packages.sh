e7c00d9c01fb3fe007661f6388bf907d9a6b62cb
depify <jim.fuller@webcomposite.com>
2014-09-12 20:44:41 +0200

    Update publish-packages.sh

--- .travis/publish-packages.sh
+++ .travis/publish-packages.sh
@@ -1,62 +1,34 @@
 #!/bin/bash
 
-###############################################################################
-# gh-pages automatic updater
-#
-# Automatically update gh-pages branch with newly generated documentation after
-# successful Travis build.
-#
-# @AUTHOR           Robert Rossmann <rr.rossmann@me.com>
-# @LICENSE          BSD-3-Clause
-# @VERSION          0.1.0
-#
-###############################################################################
+set | grep TRAVIS
+set | grep GIT_
+set | grep GH_
 
-# CONFIG
+    echo -e "Setting up for publication...\n"
 
-GH_USER="depify"
-GH_REPO="depify-websites"
-FOLDER="packages" # This is where the API documentation files reside after build
+    cp -R dist/packages.xml  $HOME/packages.xml
 
-cp -R dist/packages.xml $HOME/packages/packages.xml
+    cd $HOME
+    git config --global user.email ${GIT_EMAIL}
+    git config --global user.name ${GIT_NAME}
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${GIT_PUB_REPO} gh-pages > /dev/null
 
-# CONDITIONS
+    if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+        echo -e "Publishing specification...\n"
 
-# Only publish when running tests for PHP 5.5, when committing to master and
-# when not building a pull request
-if  [ "$TRAVIS_PULL_REQUEST" != "false" ]
-then
-  echo "Not updating gh-pages for pull request"
-  exit 0
-fi
+        TIP=${TRAVIS_TAG:="head"}
 
-# PUBLISHING
+        cd gh-pages
+        git rm -rf ./packages/${TRAVIS_BRANCH}/${TIP}
+        mkdir -p ./packages/${TRAVIS_BRANCH}/${TIP}
+        cp -Rf $HOME/packages/* ./packages/${TRAVIS_BRANCH}/${TIP}
 
-echo -e "Updating gh-pages\n"
+        git add -f .
+        git commit -m "Successful travis build $TRAVIS_BUILD_NUMBER"
+        git push -fq origin gh-pages > /dev/null
 
-# Copy data we are interested in to other place
-cp -R $FOLDER $HOME/$FOLDER
-
-# Go to $HOME and setup git
-cd $HOME
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis"
-
-# Clone gh-pages branch using Github token
-# Redirecting output to /dev/null to avoid leaking unencrypted GH_TOKEN
-git clone --quiet \
-          --branch=gh-pages \
-          https://depify@github.com/depify/depify-website.git  \
-          gh-pages > /dev/null
-
-# Copy data we are insterested in to the gh-pages directory
-cd gh-pages
-cp -Rf $HOME/$FOLDER/* .
-
-# Add, commit and push
-git add -f .
-git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-git push -fq origin gh-pages > /dev/null
-
-echo -e "Finished updating gh-pages\n"
+        echo -e "Published specification to gh-pages.\n"
+    else
+        echo -e "Publication cannot be performed on pull requests.\n"
+    fi
 
