5c235c74b5087b032a1a3bf5c64b96e2152370de
depify <jim.fuller@webcomposite.com>
2014-09-12 19:46:49 +0200

    Update publish-packages.sh

--- .travis/publish-packages.sh
+++ .travis/publish-packages.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
-
 set | grep TRAVIS
 
     echo -e "Setting up for publication...\n"
-    
-    echo -e "Setting up for publishing depify packages.xml ...\n"
 
-    cp -R dist $HOME/packages
+    cp -R dist/packages.xml $HOME/packages.xml
 
     cd $HOME
-    git config --global user.email "jim.fuller@webcomposite.com"
-    git config --global user.name "depify"
-    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/depify/depify-website gh-pages > /dev/null
+    git config --global user.email ${GIT_EMAIL}
+    git config --global user.name ${GIT_NAME}
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${GIT_PUB_REPO} gh-pages > /dev/null
 
     if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-        echo -e "Publishing depify packages.xml...\n"
+        echo -e "Publishing specification...\n"
 
         TIP=${TRAVIS_TAG:="head"}
 
         cd gh-pages
-        git rm -rf ./packages/${TRAVIS_BRANCH}/${TIP}
-        mkdir -p ./packages/${TRAVIS_BRANCH}/${TIP}
-        cp -Rf $HOME/packages/* ./packages/${TRAVIS_BRANCH}/${TIP}
+        git rm -rf ./packages.xml/${TRAVIS_BRANCH}/${TIP}
+        cp -Rf $HOME/packages.xml ./packages.xml/${TRAVIS_BRANCH}/${TIP}
 
         git add -f .
         git commit -m "Successful travis build $TRAVIS_BUILD_NUMBER"
         git push -fq origin gh-pages > /dev/null
 
-        echo -e "Published depify packages.xml to gh-pages.\n"
+        echo -e "Published specification to gh-pages.\n"
     else
         echo -e "Publication cannot be performed on pull requests.\n"
     fi
